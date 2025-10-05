febaba54325c86ed211a4e36e0135eaecc3d3a4c
Wu Tingfeng <wutingfeng@outlook.com>
2025-10-06 04:45:45 +0800

    Improve lookup table efficiency for encode/decode speedup. (#8)
    * chore: bump ci dependencies.
    
    * refactor: improve lookup table efficiency for encode/decode speedup.

--- rfc1924/tests/test_rfc1924.py
+++ rfc1924/tests/test_rfc1924.py
@@ -1,10 +1,13 @@
 from rfc1924 import encode, decode, savings
 
+
 def test_encode():
     assert encode("2345:425:2ca1::567:5673:23b5") == "AN?6(i3Y+yVr74uX@J3P"
+
 
 def test_decode():
     assert decode("AN?6(i3Y+yVr74uX@J3P") == "2345:425:2ca1::567:5673:23b5"
 
+
 def test_savings():
-    assert (savings("2345:425:2ca1::567:5673:23b5") == 'You saved 28%')
+    assert savings("2345:425:2ca1::567:5673:23b5") == "You saved 28%"
