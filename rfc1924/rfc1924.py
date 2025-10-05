5e266b4c7bf01dc9f3e9733f173de54173336e22
Wu Tingfeng <wutingfeng@outlook.com>
2025-10-05 14:56:52 +0800

    refactor: improve lookup table efficiency for encode/decode speedup.

--- rfc1924/rfc1924.py
+++ rfc1924/rfc1924.py
@@ -1,121 +1,41 @@
 from ipaddress import IPv6Address
-from functools import reduce
 
-lookup_table = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "(",
-    ")",
-    "*",
-    "+",
-    "-",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "^",
-    "_",
-    "`",
-    "{",
-    "|",
-    "}",
-    "~",
-]
+# fmt: off
+lookup_table = (
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+    "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
+    "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+    "y", "z", "!", "#", "$", "%", "&", "(", ")", "*",
+    "+", "-", ";", "<", "=", ">", "?", "@", "^", "_",
+    "`", "{", "|", "}", "~"
+)
+# fmt: on
+
+lookup_table_char_to_idx: dict[str, int] = {
+    char: idx for (idx, char) in enumerate(lookup_table)
+}
 
 
-def encode(ipv6):
-    return reduce(
-        lambda prv, nxt: prv + nxt,
-        map(
-            lambda chr: lookup_table[chr],
-            reversed(
-                [int(IPv6Address(ipv6)) // 0x55 ** cnt % 0x55 for cnt in range(0o24)]
-            ),
-        ),
+def encode(ipv6: str | int) -> str:
+    return "".join(
+        lookup_table[int(IPv6Address(ipv6)) // 0x55**cnt % 0x55]
+        for cnt in range(0o24 - 1, -1, -1)
     )
 
 
-def decode(encoded_ipv6):
+def decode(encoded_ipv6: str) -> str:
     return str(
         IPv6Address(
-            reduce(
-                lambda sum, vec: sum + vec[0] * 0x55 ** vec[1],
-                map(
-                    lambda vec: (lookup_table.index(vec[0]), vec[1]),
-                    zip(encoded_ipv6, range(0o23, -1, -1)),
-                ),
-                0,
+            sum(
+                lookup_table_char_to_idx[ch] * 0x55**exp
+                for ch, exp in zip(encoded_ipv6, range(0o23, -1, -1))
             )
         )
     )
 
 
-def savings(ipv6):
+def savings(ipv6: str) -> str:
     return f"You saved {int((1 - len(encode(ipv6)) / len(ipv6)) * 100)}%"
